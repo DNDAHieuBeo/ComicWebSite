6ed4f876dca5bf84ee08b855d59d4225d55f2161
DNDAHieuBeo <90563964+DNDAHieuBeo@users.noreply.github.com>
2023-07-12 14:13:30 +0700

    Delete ComicItem file

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getLinkImage } from "../constant/fetchData"; // Make sure the path to the fetchData file is correct
-import ComicItem from "./ComicItem";
+
 import Mainpage from "./mainpage";
 
 
